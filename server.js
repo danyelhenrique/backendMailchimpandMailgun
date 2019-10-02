e7b917aa29f9d914a9c7cc68c85f1e40d59acc00
danyelhenrique <danyelhenriquefidel@gmail.com>
2019-10-01 23:49:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const mail = require("./config/nodemailer");
+// const mail = require("./config/nodemailer");
 const chi = require("./config/mailChimp");
 const app = express();
 require("dotenv").config();
@@ -10,9 +10,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/subscribes", mail, chi, (req, res) => {
+app.post("/subscribes", chi, (req, res) => {
   res.json({ message: "Message recived Thankyou!" });
 });
+// app.post("/subscribes", mail, chi, (req, res) => {
+//   res.json({ message: "Message recived Thankyou!" });
+// });
 app.get("/", (req, res) => {
   res.json({ message: "Aplication is runing" });
 });
